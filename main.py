f1352174dedb5757582635c2094470c8f6f90aa0
Edward Jin <ejscfj@gmail.com>
2026-01-04 19:13:08 -0800

    support translation

--- main.py
+++ main.py
@@ -3,48 +3,63 @@ import threading
 import subprocess
 from AppKit import NSApplication
 from subtitles import SubtitleOverlay
+import argparse
 
 
-def read_whisper(overlay):
+def read_whisper(overlay, language_to_translate=None):
     """Read whisper-stream output and update overlay"""
+    args = [
+        "./whisper.cpp/build/bin/whisper-stream",
+        "-m",
+        (
+            "./whisper.cpp/models/ggml-large-v3-q8_0.bin"
+            if language_to_translate
+            else "./whisper.cpp/models/ggml-large-v3-turbo.bin"
+        ),
+        "-t",
+        "6",
+        "--step",
+        "1000",
+        "--length",
+        "5000",
+        "--keep",
+        "500",
+        "-c",
+        str(get_blackhole_device_index()),
+        "--subprocess-mode",
+        "-svm",
+        "./whisper.cpp/models/ggml-silero-v6.2.0.bin",
+        "--denoise",
+    ]
+
+    if language_to_translate:
+        args.append("-tr")
+        args.append("-l")
+        args.append(language_to_translate)
+
     proc = subprocess.Popen(
-        [
-            "./whisper.cpp/build/bin/whisper-stream",
-            "-m",
-            "./whisper.cpp/models/ggml-large-v3-turbo.bin",
-            "-t",
-            "6",
-            "--step",
-            "1000",
-            "--length",
-            "5000",
-            "--keep",
-            "500",
-            "-c",
-            str(get_blackhole_device_index()),
-            "--subprocess-mode",
-            "-svm",
-            "./whisper.cpp/models/ggml-silero-v6.2.0.bin",
-            "--denoise",
-        ],
+        args=args,
         stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
         text=True,
         bufsize=1,
     )
+
+    overlay.set_text("Setting up...")
 
     # give the overlay the reference so it can clean it up
     overlay.whisper_proc = proc
 
     for line in proc.stdout:
         line = line.strip()
-        if line.startswith("U:"):
-
-            text = line[2:]
+        if line.startswith("<text>:"):
+            text = line[len("<text>:") :]
             overlay.set_text(text)
+        elif line.startswith("<Ready to transcribe") or line.startswith("<error>:"):
+            overlay.set_text(line)
 
 
-def main():
+def main(language_to_translate=None):
     app = NSApplication.sharedApplication()
     request_screen_recording_permission()
 
@@ -55,7 +70,9 @@ def main():
     overlay = SubtitleOverlay.alloc().init()
     overlay.show()
 
-    threading.Thread(target=read_whisper, args=(overlay,), daemon=True).start()
+    threading.Thread(
+        target=read_whisper, args=(overlay, language_to_translate), daemon=True
+    ).start()
 
     print("Subtitle overlay started!")
     print("Drag the window to reposition. Close terminal to quit.")
@@ -67,4 +84,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-tr",
+        "--translate",
+        help="input language to translate (NOTE: output translation will always be in English)",
+    )
+    args = parser.parse_args()
+    main(language_to_translate=args.translate)
--- setup.sh
+++ setup.sh
@@ -11,15 +11,18 @@ bash autogen.sh
 make
 sudo make install
 
-cd ../whisper.cpp
+cd ../
+rm -rf rnnoise
+cd ./whisper.cpp
 
 bash ./models/download-ggml-model.sh large-v3-turbo
+bash ./models/download-ggml-model.sh large-v3
+./build/bin/quantize models/ggml-large-v3.bin models/ggml-large-v3-q8_0.bin q8_0
+rm models/ggml-large-v3.bin
 sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
 bash ./models/generate-coreml-model.sh large-v3-turbo
+bash ./models/generate-coreml-model.sh large-v3
 bash ./models/download-vad-model.sh silero-v6.2.0
 
 cmake -B build -DWHISPER_COREML=1 -DWHISPER_SDL2=ON
 cmake --build build -j --config Release
-
-cd ../
-rm -rf rnnoise
